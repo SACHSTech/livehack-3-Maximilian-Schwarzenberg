2eb3e41ee5da13e23b7c1426bd421b6fa404c59f
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2021-03-03 15:59:48 +0000

    submit

--- problem1.py
+++ problem1.py
@@ -1,1 +1,42 @@
+"""
+-------------------------------------------------------------------------------
+Name: problem1.py
+Purpose: To determine which group a player is placed in based on their wins.
 
+Author: Schwarzenberg.Maximilian
+
+Created: 03/03/2021
+------------------------------------------------------------------------------
+"""
+
+ # Define variables
+outcome = 0
+win = 0
+loss = 0
+
+print("******  Tournament Tracker ******")
+print("")
+print("Enter the wins and losses for your team:")
+
+ # for loop
+for i in range(1,7):
+  outcome = input("Game outcome (W or L): ")
+  if outcome == "W" or outcome == "w":
+    win = win + 1
+  if outcome == "L"or outcome == "l":
+    loss = loss + 1
+
+print("")
+print("Win(s): ", win)
+print("Loss(es): ", loss)
+print("")
+
+ # Win detection for group placement 
+if win == 5 or win == 6:
+  print("Your team is in Group 1")
+elif win == 3 or win == 4:
+  print("Your team is in Group 2")
+elif win == 1 or win == 2:
+  print("Your team is in Group 3")
+else:
+  print("Your team is eliminated from the tournament")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,32 @@
+"""
+-------------------------------------------------------------------------------
+Name: problem2.py
+Purpose: To enter the distance driven for each day until the total distance driven surpasses 100km and see the average and number of days it took. 
 
+Author: Schwarzenberg.Maximilian
+
+Created: 03/03/2021
+------------------------------------------------------------------------------
+"""
+
+print("***** Welcome to the DoorDash Distance Tracker ****** ")
+print("")
+
+day = 0
+distance = 0
+travelled = 0
+
+print("** Travel Log **")
+while travelled < 100:
+  distance = float(input("Enter the distance travelled for the day: "))
+  travelled = travelled + distance
+  day = day + 1
+
+average = travelled // day
+
+average = round(average,0)
+
+print("")
+print("** Summary **")
+print("It took", str(day) ,"days to surpass 100km driven.")
+print("The average distance driven per day is", str(average))
